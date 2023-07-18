b21c108a0e888ac452f740a0d204309aa74b25e0
A-bdallah <abdallah179ahmed@gmail.com>
2023-07-17 17:07:05 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 int nums;
 int nums1;
 
-for (nums = '0'; nums <= '9'; nums++)
+for (nums = '0'; nums <= '8'; nums++)
 {
 for (nums1 = '0'; nums1 <= '9'; nums1++)
 {
 putchar(nums);
 putchar(nums1);
-if (nums == '8' && nums1 == '9')
+if (nums == '9' && nums1 == '9')
 continue;
 putchar(',');
 putchar(' ');
