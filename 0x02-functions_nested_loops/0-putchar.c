9dc4f82c8dd74a97a54ab2becdd611c85f911dcb
A-bdallah <abdallah179ahmed@gmail.com>
2023-07-19 15:33:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
-*main - function prints text as output
-*
-*Return:return 0
+ * main - entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (success)
 */
 int main(void)
 {
-	char school[10] = "_putchar";
-
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
-	return (0);
+printf("_putchar\n");
+return (0);
 }
