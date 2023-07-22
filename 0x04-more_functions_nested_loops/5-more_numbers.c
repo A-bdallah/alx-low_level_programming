db4cd9f37b853b53528c5a0681162997ae4b6fa7
A-bdallah <abdallah179ahmed@gmail.com>
2023-07-21 18:59:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * more_numbers - print numbers from 0 to 14 10 times
- * Return: result (success)
-*/
+ * more_numbers - print all numbers 0 through 14 ten tiems
+ */
+
 void more_numbers(void)
-
-
-{int i;
-int j;
-
-for (j = 1; j <= 10; j++)
+{
+int i;
+int x;
+for (x = 0; x < 10; x++)
 {
 for (i = 0; i <= 14; i++)
 {
-	if (i >= 10)
-	{
-		putchar('1');
-		putchar(i % 10 + '0');
-	}
+if (i > 9)
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
