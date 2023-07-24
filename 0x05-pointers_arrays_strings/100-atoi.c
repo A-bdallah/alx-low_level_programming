33675f29773e2adefa75f6333ced2befd4e2ba48
A-bdallah <abdallah179ahmed@gmail.com>
2023-07-24 16:11:52 -0700

    100-atoi.cv

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 /**
  * _atoi - converts a string to an integer
  *
- * @s: string input parameter 
+ * @s: string input parameter
  *
  * Return: converted integer from string
 */
@@ -17,9 +17,9 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num =(num * 10) + (*s -'0');
-		else if (num >0)
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
 			break;
 	} while (*s++);
-	return (num *sign);
+	return (num * sign);
 }
