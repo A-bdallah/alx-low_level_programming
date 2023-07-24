451f4f48bf5f27243573fc947e23684f4c4110cd
A-bdallah <abdallah179ahmed@gmail.com>
2023-07-24 15:38:27 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * rev_string - check the code
+ * @s : string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+	char rev = s[0];
+	int j = 0;
+
+while (s[j] != '\0')
+j++;
+for (i = 0; i < j; i++)
+{
+	j--;
+	rev = s[i];
+	s[i] = s[j];
+	s[j] = rev;
+}
+}
