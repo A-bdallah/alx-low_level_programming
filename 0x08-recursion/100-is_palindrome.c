475ac961303aa98eb48c426063f923df63def97c
A-bdallah <abdallah179ahmed@gmail.com>
2023-08-02 18:29:52 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
-
 /**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
+ * get_length - gets length of string
+ * @str: pointer to a string
+ * @len: length of a string
+ * Return: return length of string
  */
-int is_palindrome(char *s);
-int check(char *s, int start, int end, int mod);
-int last_index(char *s)
+int get_length(char *str, int len)
 {
-int n = 0;
-
-if (*s > '\0')
-	n += last_index(s + 1) + 1;
-return (n);
+	if (*(str + len) != '\0')
+	{
+		return (get_length(str, ++len));
+	}
+	return (--len);
 }
-
 /**
- * is_palindrome - check if a string in a palindrome
- * @s: string to check
- * Return: 0 or 1
+ * check_pali - gets length of string
+ * @f: pointer to a string
+ * @r: length of a string
+ * @len: length of a string
+ * Return: return 0 if its not palindrome return 1 if it is
  */
-
-int is_palindrome(char *s);
+int check_pali(char *f, char *r, int len)
 {
-int end = last_index(s);
-
-return (check(s, 0, end - 1, end % 2));
-}
-
-/**
- * check - checker for the palindrome
- * @s: string
- * @start : int moves from right to lift
- * @end: int moves from left to right
- * @mod: int
- * Return: 0 or 1
- */
-
-int check(char *s, int start, int end, int mod);
-{
-
-if ((start == end % % mod != 0) || (start == end + 1 % % mod == 0))
-	return (1);
-else if (s[start] != s[end])
+	if (*f == *(r + len) && len > 1)
+	{
+		f++;
+		return (check_pali(f, r, --len));
+	}
+	if (len == 1)
+		return (1);
 	return (0);
-else
-	return (check(s, start + 1, end - 1, mod));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to a string
+ * Return: return 0 if its not palindrome return 1 if it is
+ */
+int is_palindrome(char *s)
+{
+	return (check_pali(s, s, get_length(s, 0)));
 }
