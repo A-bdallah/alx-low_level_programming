afb21bd990451a18a124b977812fbbafd5ed57ce
A-bdallah <abdallah179ahmed@gmail.com>
2023-08-08 01:27:18 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,69 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * _atoi - convert string to an integer
- * @s: string to be converted
- * Return:always 0 (success)
-*/
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-if (s[i] >= '0' && s[i] <= '9')
-{
-	digit = s[i] - '0';
-	if (d % 2)
-		digit = -digit;
-	n = n * 10 + digit;
-	f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-	f = 0;
-	}
-i++;
-}
-if (f == 0)
-{	return (0);
-
-}
-	return (n);
-}
-
 
 /**
- * main -  that multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return:always 0 (success)
-*/
+ * main - function that multiplies two numbers
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if runs
+ */
+
 int main(int argc, char *argv[])
 {
-	int r, num1, num2;
+	int x, y;
 
-	if (argc < 3 || argc > 3)
-	{
+	if (argc != 3)
 		printf("Error\n");
-		return (1);
+	else
+	{
+		x = strtol(argv[1], 0, 10);
+		y = strtol(argv[2], 0, 10);
+		printf("%d\n", x * y);
 	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	r = num1 * num2;
-
-	printf("%d\n", r);
 	return (0);
 }
